be55b1fd99038b94afbdeacf5eb96e94a83a2e2a
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:46:40 +0530

    Add files via upload

---
+++ floatinput.java
@@ -0,0 +1,18 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class floatinput {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+		float digit=input.nextFloat();
+		System.out.println(digit);
+		
+
+	}
+
+}
